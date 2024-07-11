377b83178394d5809b24a1e917204f577463372f
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-07-11 14:38:58 +0200

    variable names added..

--- aws/ec2/awsec2.tf
+++ aws/ec2/awsec2.tf
@@ -1,15 +1,64 @@
 provider "aws" {
   region     = "eu-west-3"
-  access_key = "XXXXX"
-  secret_key = "XXXXXX"
+  access_key = "XXXXXX"
+  secret_key = "XXXX"
 }
 
 resource "aws_instance" "my-first-aws-instance" {
-  ami           = "ami-09d83d8d719da9808"
-  instance_type = "t2.micro"
+  ami                         = "ami-09d83d8d719da9808"
+  instance_type               = var.instanceType
+  count                       = var.instanceCount
+  associate_public_ip_address = var.enable_public_ip
+  tags                        = var.project_environment
 
-  tags = {
-    Name = "My-EC2"
+}
+
+resource "aws_iam_user" "users" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+
+
+
+
+
+//inline variables are listed below.
+
+//string
+variable "instanceType" {
+  type        = string
+  default     = "t2.micro"
+  description = "Instance type of the resource."
+}
+
+//number
+variable "instanceCount" {
+  type        = number
+  default     = 2
+  description = "No:of instance need to create."
+}
+
+//boolean
+variable "enable_public_ip" {
+  type        = bool
+  default     = true
+  description = "Enable public IP for the new resource."
+}
+
+//list
+variable "user_names" {
+  type        = list(string)
+  default     = ["Anoop@1987", "Manasa@1991", "Ishaan@2020"]
+  description = "IAM Users"
+}
+
+//map
+variable "project_environment" {
+  type        = map(string)
+  description = "Project name and environment."
+  default = {
+    "project"     = "project-alpha",
+    "environment" = "dev"
   }
-
 }
