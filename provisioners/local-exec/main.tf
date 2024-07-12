1a69d812f23232e32777adf19b3a9bf54fb27eae
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-07-12 17:15:40 +0200

    provisioners : local-exec

---
+++ provisioners/local-exec/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "eu-west-3"
+  access_key = var.accesskey
+  secret_key = var.secretkey
+}
+
+resource "aws_instance" "ec2-instance" {
+  ami           = "ami-09d83d8d719da9808"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "File Provisioners Test"
+  }
+
+  // this file will be created locally.
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> ./output/private_ips.txt"
+  }
+
+}
---
+++ provisioners/local-exec/variable.tf
@@ -0,0 +1,9 @@
+variable "accesskey" {
+  type        = string
+  description = "Access Key"
+}
+
+variable "secretkey" {
+  type        = string
+  description = "Secret Key"
+}
