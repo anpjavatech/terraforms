a1a25e3d2d858904062daad049bcb52d488f1f7c
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-07-18 17:50:33 +0200

    Revert "Revert "validation of variables""
    This reverts commit 95c8f2719725ceab32ccd1c6b9efecbdec28c41a.

---
+++ validation/variable.tf
@@ -0,0 +1,19 @@
+variable "accesskey" {
+  type        = string
+  description = "Access Key"
+}
+
+variable "secretkey" {
+  type        = string
+  description = "Secret Key"
+}
+
+variable "instanceType" {
+  type    = string
+  default = "t2.nano"
+
+  validation {
+    condition     = can(regex("^[Tt][2-3].(micro|small)", var.instanceType))
+    error_message = "Invalid Instance type. You can only choose - t2.micro, t2.small"
+  }
+}
